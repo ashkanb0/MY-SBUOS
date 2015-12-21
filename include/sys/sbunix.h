db362d66ddf25d7c89a666d60560c86cb3c33544
ashkan <ashkan.dant3@gmail.com>
2015-12-21 17:38:02 -0500

    starting some work/3

--- include/sys/sbunix.h
+++ include/sys/sbunix.h
@@ -4,11 +4,63 @@
 #include <sys/defs.h>
 #include <stdarg.h>
 
+
+int itoh_printf(char* buf, uint64_t n){
+	if (n==0){
+		buf[0]='0';
+		buf[1]=0;
+		return 0;
+	}
+	uint64_t temp = n;
+	int i=0;
+	while(temp){
+		i++;
+		temp/=16;
+	}
+	buf[i] = '\0';
+	for(i--;i>=0;i--){
+		temp = n%16;
+		if (temp>9)
+			buf[i]='A'+(temp-10);
+		else
+			buf[i]='0'+temp;
+		n/=16;
+	}
+	return 0;
+}
+
+int itoa_printf(char* buf, uint64_t n){
+	if (n==0){
+		buf[0]='0';
+		buf[1]=0;
+		return 0;
+	}
+	int temp = n;
+	int i=0;
+	while(temp){
+		i++;
+		temp/=10;
+	}
+	buf[i] = '\0';
+	for(i--;i>=0;i--){
+		buf[i]='0'+(n%10);
+		n/=10;
+	}
+	return 0;
+}
+
+char _buffer [30];
+
+
+
+
 long row = 0; long col = 0;
 
 void write_k(char v){
 	char* position = (char*)(0xb8000 + 160*row+ 2*col);
 	*position = v;
+	position++;
+	*position = 0;
 	col ++;
 	if (col>=80){
 		col = 0;
@@ -24,60 +76,47 @@ void printf(const char *fmt, ...){
 	
 	va_start(val, fmt);
 
-	// char *strArray = _buffer; 
+	// char *_buffer = _buffer; 
 
 	while(*fmt) {
-		// if(*fmt == '%') {
-		// 	++fmt;
-		// 	if(*fmt == 'c') {
-		// 		_buffer[0] = va_arg(val, int);
-		// 		_buffer[1] = 0;  
-		// 		write(1, _buffer, 1);
-		// 		printed++;
-		// 	}
-		// 	else if(*fmt == 'd') {
-		// 		int number = va_arg(val, int);
-		// 		if (number<0){
-		// 			strArray[0] = '-';
-		// 			itoa_printf(strArray+1, -number); 
-		// 		}else
-		// 			itoa_printf(strArray, number); 
-		// 		while(*strArray)
-		// 		{
-		// 			write(1, strArray, 1);
-		// 			strArray++;
-		// 			printed++;
-		// 		}
+		if(*fmt == '%') {
+			++fmt;
+			if(*fmt == 'c') {
+				_buffer[0] = va_arg(val, int);
+				_buffer[1] = 0;  
+				write_k(_buffer[0]);
+			}
+			else if(*fmt == 'd') {
+				int number = va_arg(val, int);
+				if (number<0){
+					_buffer[0] = '-';
+					itoa_printf(_buffer+1, -number); 
+				}else
+					itoa_printf(_buffer, number); 
+				printf(_buffer);
 
-		// 	}
-		// 	else if(*fmt == 'x') {
-		// 		int number = va_arg(val, int);
-		// 		itoh_printf(strArray, number); 
-		// 		while(*strArray)
-		// 		{
-		// 			write(1, strArray, 1);
-		// 			strArray++;
-		// 			printed++;
-		// 		}
-
-		// 	}
-		// 	else if(*fmt == 's') {
-		// 		char *strTemp;
-		// 		strTemp = va_arg(val, char*);
-		// 		_buffer[1] = 0;  
-		// 		while(*strTemp != '\0')
-		// 		{
-		// 			_buffer[0] = *strTemp;
-		// 			write(1, _buffer, 1);
-		// 			printed++;
-		// 			strTemp++;
-		// 		}
-		// 	}
-		// }
-		// else{
+			}
+			else if(*fmt == 'x') {
+				int number = va_arg(val, int);
+				itoh_printf(_buffer, number); 
+				printf(_buffer);
+			}
+			else if(*fmt == 's') {
+				char *strTemp;
+				strTemp = va_arg(val, char*);
+				_buffer[1] = 0;  
+				while(*strTemp != '\0')
+				{
+					_buffer[0] = *strTemp;
+					write_k(_buffer[0]);
+					strTemp++;
+				}
+			}
+		}
+		else{
 			
 			write_k(*fmt);
-		// }
+		}
 		++fmt;
 	}
 
