a1f489df2b1a222e924defd6bb2af3cf2095ce3f
ashkan <ashkan.dant3@gmail.com>
2015-12-20 20:33:28 -0500

    start

---
+++ include/sys/defs.h
@@ -0,0 +1,17 @@
+#ifndef _DEFS_H
+#define _DEFS_H
+
+#define NULL ((void*)0)
+
+typedef unsigned long  uint64_t;
+typedef          long   int64_t;
+typedef unsigned int   uint32_t;
+typedef          int    int32_t;
+typedef unsigned short uint16_t;
+typedef          short  int16_t;
+
+typedef uint64_t size_t;
+typedef uint32_t pid_t;
+typedef uint64_t off_t;
+
+#endif
