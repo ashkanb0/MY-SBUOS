0739168ce78ed774548735e9e75e94fa88c5bc9a
ashkan <ashkan.dant3@gmail.com>
2015-12-26 00:05:30 -0500

    more and more tests

--- sys/main.c
+++ sys/main.c
@@ -24,11 +24,11 @@ void start(uint32_t* modulep, void* physbase, void* physfree)
 	idts_setup();
 	PIC_setup();
 
-	// __asm__ volatile("sti");
-	__asm__ volatile("int $0");
-	__asm__ volatile("int $0");
-	__asm__ volatile("int $0");
-	__asm__ volatile("int $0");
+	__asm__ volatile("sti");
+	// __asm__ volatile("int $0");
+	// __asm__ volatile("int $0");
+	// __asm__ volatile("int $0");
+	// __asm__ volatile("int $0");
 	while(1){
 	}
 }
