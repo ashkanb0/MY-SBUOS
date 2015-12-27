1f2122708ab22af7f536af698b648bffeb3cf9d2
ashkan <ashkan.dant3@gmail.com>
2015-12-27 15:53:07 -0500

    starting to work with kbd/12

--- sys/main.c
+++ sys/main.c
@@ -25,7 +25,7 @@ void start(uint32_t* modulep, void* physbase, void* physfree)
 	PIC_setup();
 
 	__asm__ volatile("sti");
-	// __asm__ volatile("int $0");
+	__asm__ volatile("int $0");
 	// __asm__ volatile("int $0");
 	// __asm__ volatile("int $0");
 	// __asm__ volatile("int $0");
