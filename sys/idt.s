861c677e0089b00519888f9dfb68ecdb34c89822
ashkan <ashkan.dant3@gmail.com>
2015-12-27 16:49:01 -0500

    trying to get keyboard to send intr/2

--- sys/idt.s
+++ sys/idt.s
@@ -6,19 +6,28 @@
 interrupt_0_handler:
 	# pushad
 	# TODO : save rax, rdi, rsi, rdx, rcx, r8, r9, r10, r11
-	# push %ax
-	# push %bx
-	# push %cx
-	# push %dx
+	push %ax
+	push %di
+	push %si
+	push %dx
+	push %cx
+	push %r8
+	push %r9
+	push %r10
+	push %r11
 	# pushf
 	# cld
 	call int_0_srv
 	# popad
-	# pop %dx
-	# pop %cx
-	# pop %bx
-	# pop %ax
-	# popf
+	pop %r11
+	pop %r10
+	pop %r9
+	pop %r8
+	pop %cx
+	pop %dx
+	pop %si
+	pop %di
+	pop %ax
 	iretq
 
 .globl keyboard_interrupt_handler
