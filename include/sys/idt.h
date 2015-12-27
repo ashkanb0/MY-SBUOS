27298b9c8ef6dc4ba07c0ccf324fbf3e8c0182a7
ashkan <ashkan.dant3@gmail.com>
2015-12-27 18:12:01 -0500

    trying to get keyboard to send intr/4

--- include/sys/idt.h
+++ include/sys/idt.h
@@ -25,7 +25,7 @@ void set_isr(IDTDescriptor* idt, int int_num, uint64_t handler){
 	idt[int_num].zero1= 0;
 	idt[int_num].zero2= 0;
 	idt[int_num].selector= 0x0008;
-	idt[int_num].type_attr= 0x0E;
+	// idt[int_num].type_attr= 0x0E;
 
 }
 
