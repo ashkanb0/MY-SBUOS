9338425d71d1890548b3bb19e9d4043c392830d2
ashkan <ashkan.dant3@gmail.com>
2015-12-27 18:34:50 -0500

    trying to get keyboard to send intr/5

--- sys/idt.s
+++ sys/idt.s
@@ -6,6 +6,7 @@
 interrupt_0_handler:
 	# pushad
 	# TODO : save rax, rdi, rsi, rdx, rcx, r8, r9, r10, r11
+	cli
 	push %ax
 	push %di
 	push %si
@@ -29,6 +30,7 @@ interrupt_0_handler:
 	pop %di
 	pop %ax
 	# TODO : iretq ?
+	sti
 	iretq
 
 .globl keyboard_interrupt_handler
@@ -36,18 +38,29 @@ interrupt_0_handler:
 keyboard_interrupt_handler:
 	# pushad
 	# pushf
+	cli
+	push %ax
+	push %di
+	push %si
+	push %dx
+	push %cx
+	push %r8
+	push %r9
+	push %r10
+	push %r11
+	# pushf
 	# cld
-	# push %ax
-	# push %bx
-	# push %cx
-	# push %dx
 	call int_kbd_srv
-	# TODO : send EOI
-	# pop %dx
-	# pop %cx
-	# pop %bx
-	# pop %ax
-	# popf
 	# popad
-
+	pop %r11
+	pop %r10
+	pop %r9
+	pop %r8
+	pop %cx
+	pop %dx
+	pop %si
+	pop %di
+	pop %ax
+	
+	sti
 	iretq
