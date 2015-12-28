fd60a532df2219ab538fba5e239155c8bc157176
ashkan <ashkan.dant3@gmail.com>
2015-12-27 23:49:26 -0500

    finding_out the int number/ bs :))

--- sys/idt.s
+++ sys/idt.s
@@ -1,70 +1,70 @@
 .text
 
-.globl dummy_interrupt_handler_0
-.align 4
+# .globl dummy_interrupt_handler_0
+# .align 4
  
-dummy_interrupt_handler_0:
-	# pushad
-	# TODO : save rax, rdi, rsi, rdx, rcx, r8, r9, r10, r11
-	cli
-	push %ax
-	push %di
-	push %si
-	push %dx
-	push %cx
-	push %r8
-	push %r9
-	push %r10
-	push %r11
-	# pushf
-	# cld
-	call int_dmmy_srv_0
-	# popad
-	pop %r11
-	pop %r10
-	pop %r9
-	pop %r8
-	pop %cx
-	pop %dx
-	pop %si
-	pop %di
-	pop %ax
-	# TODO : iretq ?
-	sti
-	iretq
+# dummy_interrupt_handler_0:
+# 	# pushad
+# 	# TODO : save rax, rdi, rsi, rdx, rcx, r8, r9, r10, r11
+# 	cli
+# 	push %ax
+# 	push %di
+# 	push %si
+# 	push %dx
+# 	push %cx
+# 	push %r8
+# 	push %r9
+# 	push %r10
+# 	push %r11
+# 	# pushf
+# 	# cld
+# 	call int_dmmy_srv_0
+# 	# popad
+# 	pop %r11
+# 	pop %r10
+# 	pop %r9
+# 	pop %r8
+# 	pop %cx
+# 	pop %dx
+# 	pop %si
+# 	pop %di
+# 	pop %ax
+# 	# TODO : iretq ?
+# 	sti
+# 	iretq
 
-.globl dummy_interrupt_handler_1
-.align 4
+# .globl dummy_interrupt_handler_1
+# .align 4
  
-dummy_interrupt_handler_1:
-	# pushad
-	# TODO : save rax, rdi, rsi, rdx, rcx, r8, r9, r10, r11
-	cli
-	push %ax
-	push %di
-	push %si
-	push %dx
-	push %cx
-	push %r8
-	push %r9
-	push %r10
-	push %r11
-	# pushf
-	# cld
-	call int_dmmy_srv_1
-	# popad
-	pop %r11
-	pop %r10
-	pop %r9
-	pop %r8
-	pop %cx
-	pop %dx
-	pop %si
-	pop %di
-	pop %ax
-	# TODO : iretq ?
-	sti
-	iretq
+# dummy_interrupt_handler_1:
+# 	# pushad
+# 	# TODO : save rax, rdi, rsi, rdx, rcx, r8, r9, r10, r11
+# 	cli
+# 	push %ax
+# 	push %di
+# 	push %si
+# 	push %dx
+# 	push %cx
+# 	push %r8
+# 	push %r9
+# 	push %r10
+# 	push %r11
+# 	# pushf
+# 	# cld
+# 	call int_dmmy_srv_1
+# 	# popad
+# 	pop %r11
+# 	pop %r10
+# 	pop %r9
+# 	pop %r8
+# 	pop %cx
+# 	pop %dx
+# 	pop %si
+# 	pop %di
+# 	pop %ax
+# 	# TODO : iretq ?
+# 	sti
+# 	iretq
 
 .globl keyboard_interrupt_handler
 .align 4
