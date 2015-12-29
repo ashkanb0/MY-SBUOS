ff79d855376ace299b0d8256d688e527b21a4a0c
ashkan <ashkan.dant3@gmail.com>
2015-12-29 14:13:51 -0500

    starting with the memory

--- sys/main.c
+++ sys/main.c
@@ -13,7 +13,7 @@ void start(uint32_t* modulep, void* physbase, void* physfree)
 		uint32_t type;
 	}__attribute__((packed)) *smap;
 	while(modulep[0] != 0x9001) modulep += modulep[1]+2;
-	printf("physbase = [%x]\n", physbase);
+	printf("physbase = [%x]\n", *((long*)physbase));
 	printf("physfree = [%x]\n", physfree);
 	for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
 		if (smap->type == 1 /* memory */ && smap->length != 0) {
