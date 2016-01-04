314f97c75d4f0114cc8a32c78f2b82e2ae2784c3
ashkan <ashkan.dant3@gmail.com>
2016-01-04 18:08:50 -0500

    starting tarfs

--- sys/tarfs.c
+++ sys/tarfs.c
@@ -4,7 +4,7 @@ uint64_t _tar_start, _tar_end;
 
 uint64_t atoi(int l, char* s){
 	uint64_t res = 0;
-	for(int i=0; i<l; i++, s++){
+	for(int i=0; i<l-1; i++, s++){
 		res = (res* 10) + ((uint64_t)*s - 0x30);
 	}
 	return res;
