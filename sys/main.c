921bd45bea00a3785046d3ce4f65881ee2d06462
ashkan <ashkan.dant3@gmail.com>
2016-01-04 16:40:50 -0500

    starting tarfs

--- sys/main.c
+++ sys/main.c
@@ -38,9 +38,9 @@ void start(uint32_t* modulep, void* physbase, void* physfree)
 
 	for (int i = 0; i < 10; ++i)
 	{
-		tarfs_header* p = (tarfs_header *) (_tar_start+sizeof(tarfs_header)+offset);
+		tarfs_header* p = (tarfs_header *) (_tar_start+offset);
 		printf("name: %s, size: %s \n", p->name, p->size);
-		offset += atoi(p->size);
+		offset += atoi(p->size)+sizeof(tarfs_header);
 	}
 	// kernel starts here
 	idts_setup();
