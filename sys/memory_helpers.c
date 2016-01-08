366a7384a69c23b031c5eb343d82955c39be4325
ashkan <ashkan.dant3@gmail.com>
2016-01-08 03:47:17 -0500

    mapping kernel mem

--- sys/memory_helpers.c
+++ sys/memory_helpers.c
@@ -104,7 +104,7 @@ void filter_out_pages(uint64_t base, uint64_t top){
 
 void * make_pages(uint64_t base, uint64_t length, void * physfree){
 	// JK :))))
-	// if(base==0)return physfree;
+	if(base==0)return physfree;
 	uint64_t free_int = (uint64_t) physfree;
 	free_int = free_int%PAGESIZE == 0? free_int : free_int + PAGESIZE - (free_int% PAGESIZE);
 	// I'll throw this out! 
