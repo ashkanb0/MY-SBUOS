6f492b130de6d0ba6420cc18913da05e0325d5d2
ashkan <ashkan.dant3@gmail.com>
2016-01-14 02:54:05 -0500

    changing the system of paging queues/ debug 17

--- sys/memory_helpers.c
+++ sys/memory_helpers.c
@@ -105,10 +105,10 @@ void map_v(uint64_t phys, uint64_t virt, uint64_t* table, int lvl){
 	}
 	// TODO : use virtual memory of (table[index])
 	// map_v(phys, virt, (uint64_t*)(virt_mem(table[index] & ~3, lvl)), lvl - 1);
-	if (self_referencing_enabled==0)
-		map_v(phys, virt, (uint64_t*)(table[index] & ~3), lvl - 1);
-	else
-		map_v(phys, virt, get_page_table(virt, lvl - 1), lvl - 1);
+	// if (self_referencing_enabled==0)
+	// 	map_v(phys, virt, (uint64_t*)(table[index] & ~3), lvl - 1);
+	// else
+	map_v(phys, virt, get_page_table(virt, lvl - 1), lvl - 1);
 }
 
 uint64_t mem_map_v(uint64_t base, uint64_t end, uint64_t vrtlmm, uint64_t table){
@@ -194,6 +194,9 @@ void setup_paging(
 	
 	self_referencing_enabled = 0;
 
+	uint64_t boot_cr3 = _read_cr3();
+	((uint64_t*)boot_cr3)[511] = boot_cr3|3;
+
 	filter_out_pages((uint64_t)physbase, (uint64_t)physfree); // kernel
 	filter_out_pages(0xb8000 - PAGESIZE, 0xbb200); // mem-mapped display // TODO: is this correct?
 	
@@ -211,18 +214,6 @@ void setup_paging(
 	kernel_vrt =  mem_map_v((uint64_t)physbase, (uint64_t)physfree, kernel_vrt, kernel_pml4->base);
 	mem_map_v((uint64_t)displaybase, (uint64_t)displayfree, kernel_vrt, kernel_pml4->base);
 
-	// DEBUGGING, MAKE SURE TO REMOVE IT!!
-	printf("CR0 %x\n", _read_cr0());
-	printf("CR3 %x\n", _read_cr3());
-	printf("CR4 %x\n", _read_cr4());
-
-
-
-	// _free_page_list_head = _free_page_list_head|KERNEL_MAPPING;
-	// _free_page_list_tail = _free_page_list_tail|KERNEL_MAPPING;
-	// _used_page_list_head = _used_page_list_head|KERNEL_MAPPING;
-	// _used_page_list_tail = _used_page_list_tail|KERNEL_MAPPING;
-
 	_page_list = (mem_page *)(((uint64_t)_page_list)|KERNEL_MAPPING);
 
 	set_display_address(kernel_vrt| KERNEL_MAPPING);
@@ -230,14 +221,9 @@ void setup_paging(
 	self_referencing_enabled = 1;
 
 	// CHANEG PAGING
-	// _disable_paging();
 	_set_cr3((uint64_t)kernel_pml4->base);
-	// _enable_paging();
 
 	printf("HELLO PAGED WORLD!\n");
-	// DEBUGGING, MAKE SURE TO REMOVE IT!!
-	// enabling paging
-	// _enable_paging();
 
 }
 
