7025f2f274ffade868b5261c48f757612a4f3dc3
ashkan <ashkan.dant3@gmail.com>
2016-02-05 01:40:07 -0500

    let's run init/7

--- sys/sysutil.c
+++ sys/sysutil.c
@@ -3,7 +3,7 @@
 int kstrlen(char* s){
 	int i;
 	for(i=0; s[i]; i++);
-	return i+1; 
+	return i;
 }
 
 void kstrcpy(char* dest, char* src, int size){
@@ -18,7 +18,7 @@ void kstrcpy(char* dest, char* src, int size){
 int kstrcmp(char* s1, char* s2){
 	int l1 = kstrlen(s1);
 	int l2 = kstrlen(s2);
-	if (l1!=l2)return s1[l1]-s2[l2];
+	if (l1!=l2)return s1[l1]-s2[l1];
 	for(int i=0; s1[i]; i++){
 		if(s1[i]!=s2[i]) return s1[i]-s2[i];
 	}
