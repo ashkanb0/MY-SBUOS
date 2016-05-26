9668b2e7da5ebd76018eeafca7b5d5f611c76803
ashkan <ashkan.dant3@gmail.com>
2016-05-26 18:39:47 -0400

    let's see what are we dealing with/5

--- sys/process_helpers.c
+++ sys/process_helpers.c
@@ -111,9 +111,9 @@ void schedule(){
 
 	__asm__ volatile(
 		"push $0x23\n\t"
-		"push %1\n\t"
-		"push $0x0000001b\n\t"
 		"push %0\n\t"
+		"push $0x1b\n\t"
+		"push %1\n\t"
 		"iretq\n\t"
 		:
 		: "r"(prog->ip), "r"(prog->sp)
