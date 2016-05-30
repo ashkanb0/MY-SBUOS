5f0fdfa18de8ab1d8898fefda781cdca7700e4de
ashkan <ashkan.dant3@gmail.com>
2016-05-29 20:35:39 -0400

    let's see what are we dealing with/7

--- sys/process_helpers.c
+++ sys/process_helpers.c
@@ -110,12 +110,12 @@ void schedule(){
 	__asm__ volatile("ltr %ax");
 
 		// "push $0x33\n\t"
-		// "push $0x43\n\t"
-		// "push $0x1b\n\t"
+		// "pushf\n\t"
 	__asm__ volatile(
 		"push $0x23\n\t"
 		"push %1\n\t"
-		"pushf\n\t"
+		"push $0x1b\n\t"
+		"push $0x43\n\t"
 		"push %0\n\t"
 		"iretq\n\t"
 		:
