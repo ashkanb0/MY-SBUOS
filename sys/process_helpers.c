1fa228c46bb28262a07a4e1d4503832bd75cf627
ashkan <ashkan.dant3@gmail.com>
2016-06-04 16:31:45 -0400

    steps through the misty woods/5

--- sys/process_helpers.c
+++ sys/process_helpers.c
@@ -116,6 +116,8 @@ void init(){
 	// *(pcbb->kernel_sp) = (uint64_t)(_active_pcb);
 	pcbb->kernel_sp --;
 	*(pcbb->kernel_sp) = (uint64_t)(k_thread_B);
+	pcbb->kernel_sp --;
+	*(pcbb->kernel_sp) = (uint64_t)(k_thread_B);
 
 	enqueue_process(&processq, pcba);
 	enqueue_process(&processq, pcbb);
