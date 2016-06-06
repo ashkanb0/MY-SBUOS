4ed93014c6eb725ab65bc2d5a0bed496e97ef145
ashkan <ashkan.dant3@gmail.com>
2016-06-05 20:23:54 -0400

    get the user running/1

--- sys/process_helpers.c
+++ sys/process_helpers.c
@@ -79,22 +79,9 @@ void k_thread_0(){
 	}
 }
 
-void _switch_to_ring_3(){
-	// http://wiki.osdev.org/Getting_to_Ring_3
-	// tss.rsp0 = (uint64_t) (_active_pcb -> kernel_stack + PAGESIZE - 16);
-	__asm__ volatile(
-		"push 0x23\n\t"
-		// "push %0\n\t"
-		// "pushf\n\t"
-		// "push 0x1b\n\t"
-		// "push %1\n\t"
-		// "iretq\n\t"
-		:
-		: "r"(_active_pcb->user_sp), "r"(_active_pcb->ip)
-	);
-	printf("HUH!\n");
-	while(1);
-}
+// void _switch_to_ring_3(){
+	
+// }
 
 void k_thread_kernel(){
 	
@@ -103,7 +90,20 @@ void k_thread_kernel(){
 	}
 	//just an extra check! It should always be true!
 	if (_active_pcb -> status == RUNNING){
-		_switch_to_ring_3();	
+		// http://wiki.osdev.org/Getting_to_Ring_3
+		tss.rsp0 = (uint64_t) (_active_pcb -> kernel_stack + PAGESIZE - 16);
+		__asm__ volatile(
+			"push 0x23\n\t"
+			"push %0\n\t"
+			"pushf\n\t"
+			"push 0x1b\n\t"
+			"push %1\n\t"
+			"iretq\n\t"
+			:
+			: "r"(_active_pcb->user_sp), "r"(_active_pcb->ip)
+		);
+		printf("HUH!\n");
+		while(1);	
 	}
 	printf("[k_thread_kernel]: Shouldn't have gotten here!! PANIC!!!!\n");
 	while(1);
