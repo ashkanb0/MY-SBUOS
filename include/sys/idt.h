95cc72ee2ff33bb852e295514139be526c8cd8c4
ashkan <ashkan.dant3@gmail.com>
2016-06-10 11:23:01 -0400

    can I see it?/2

--- include/sys/idt.h
+++ include/sys/idt.h
@@ -68,12 +68,12 @@ void int_syscall_srv(){
 
 void protection_failure_interrupt_handler(void);
 void int_prtctn_srv(exception_stack stack){
-	uint64_t address = 0;
+	// uint64_t address = 0;
 	uint32_t error = stack.error;
 
-	__asm__ volatile("movq %%cr2, %0":"=r"(address):);
+	// __asm__ volatile("movq %%cr2, %0":"=r"(address):);
 
-	printf("(general protection failure): %x: %x\n",address, error);
+	printf("(general protection failure): %x\n", error);
 	while(1);
 	// TODO: 
 	// k_process_exit();
