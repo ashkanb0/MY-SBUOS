7f0cef977a5ac1c46a76c1b018d0cf66b4e68235
ashkan <ashkan.dant3@gmail.com>
2016-06-11 16:14:41 -0400

    can I see it?/2

--- sys/systemcall.c
+++ sys/systemcall.c
@@ -1,9 +1,24 @@
 #include <sys/systemcall.h>
+#include <sys/syscall.h>
 
 uint64_t do_system_call(uint64_t syscall_code, uint64_t arg1, uint64_t arg2, uint64_t arg3){
 	printf("syscall_code: %x, arg1, %x, arg2, %x, arg3, %x\n",syscall_code, arg1, arg2, arg3);
-	while(1);
-	return 0;
+	
+	switch(syscall_code)
+	{
+		case SYS_exit : printf("SYS_exit NOT IMPLEMENTED\n");
+						while(1);
+						break;
+		case SYS_brk : printf("SYS_brk NOT IMPLEMENTED\n");
+						while(1);
+						break;
+		default : printf("SYSCALL NOT IMPLEMENTED: %d, 0x%x\n", syscall_code, syscall_code);
+						while(1);
+						break;
+
+	}
+
+
 }
 
 
