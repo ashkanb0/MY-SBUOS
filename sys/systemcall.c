92ec7e8cc45462b176fce52af8868f8a7129da0e
ashkan <ashkan.dant3@gmail.com>
2016-06-11 18:48:22 -0400

    implement break

--- sys/systemcall.c
+++ sys/systemcall.c
@@ -15,7 +15,7 @@ uint64_t do_break (uint64_t arg1){
 
 uint64_t do_getcwd (uint64_t buffer, uint64_t size){
 	pcb* proc = get_active_pcb();
-	int i = kstrcpy(buffer, proc-> wd);
+	int i = kstrcpy((char*)buffer, proc-> wd, size);
 	if (i == -1) return 0;
 	return buffer;
 }
@@ -29,7 +29,7 @@ uint64_t do_system_call(uint64_t syscall_code, uint64_t arg1, uint64_t arg2, uin
 						break;
 		case SYS_brk : return do_break(arg1);
 						break;
-		case SYS_getcwd : return do_getcwd(arg1);
+		case SYS_getcwd : return do_getcwd(arg1, arg2);
 						break;
 		default : printf("SYSCALL NOT IMPLEMENTED: %d, 0x%x\n", syscall_code, syscall_code);
 						while(1);
