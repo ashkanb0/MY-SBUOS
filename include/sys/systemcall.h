5c615feaf0fe4364a9e520fc8b6734afe2f0e321
ashkan <ashkan.dant3@gmail.com>
2016-06-11 16:03:28 -0400

    can I see it?/2

--- include/sys/systemcall.h
+++ include/sys/systemcall.h
@@ -5,6 +5,6 @@
 #include <sys/sbunix.h>
 
 
-uint64_t do_system_call(uint64_t syscall_code, uint64_t arg1, uint64_t arg2, uint64_t agr3);
+uint64_t do_system_call(uint64_t syscall_code, uint64_t arg1, uint64_t arg2, uint64_t arg3);
 
 #endif
