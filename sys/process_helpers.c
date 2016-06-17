9f4dbd73368bfb4a20c7d2bb8f8b4028684d1907
ashkan <ashkan.dant3@gmail.com>
2016-06-16 22:15:12 -0400

    implement fork/4

--- sys/process_helpers.c
+++ sys/process_helpers.c
@@ -115,6 +115,8 @@ pcb* get_forked_pcb(pcb* parent){
 	child -> user_brk_point = parent -> user_brk_point;
 	child -> user_sp = parent -> user_sp;
 	
+	*(((uint64_t*)(child -> user_sp))+1) = 0x0; 
+
 	kstrcpy (child-> pname, parent-> pname, 50);
 	kstrcpy (child-> wd, parent-> wd, 50);
 
