af7ae2154160e792b199f71eb2f413d03f88572a
ashkan <ashkan.dant3@gmail.com>
2016-06-16 22:37:14 -0400

    implement fork/4

--- sys/memory_helpers.c
+++ sys/memory_helpers.c
@@ -344,7 +344,7 @@ uint64_t map_page_COW (uint64_t phys, uint64_t virt, uint64_t flags, uint64_t pi
 			// create it!
 			mem_page* next_lvl_page = get_free_page();
 			vma_register_page(next_lvl_page, pid);
-			uint64_t* copy_from = (uint64_t*)(table[index]);
+			uint64_t* copy_from = (uint64_t*)(table[index]&0xffffffffff000);
 			for(int i = 0; i<512; i++)
 				temp_page[i] = copy_from[i];
 			table[index] = ((uint64_t)next_lvl_page->base|PRESENT|READ_WRITE|USER_ACCESSIBLE); 
