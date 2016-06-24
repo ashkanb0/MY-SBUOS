ffdf5d2008fa89737462cb8a60fcda813714f9ba
ashkan <ashkan.dant3@gmail.com>
2016-06-24 02:58:15 -0400

    debugging

--- include/syscall.h
+++ include/syscall.h
@@ -35,7 +35,7 @@ static __inline int64_t syscall_0(uint64_t n) {
 		"movq %%rax, %0\n\t"
 		 : "=r" (res)
 		 : "r" (n)
-		 : "rax", "rdi", "rsi", "rdx", "memory"
+		 : "rax"
 		 );
 	return res;
 }
@@ -51,7 +51,7 @@ static __inline int64_t syscall_1(uint64_t n, uint64_t a1) {
 		"movq %%rax, %0\n\t"
 		 : "=r" (res)
 		 : "r" (n), "r" (a1)
-		 : "rax", "rdi", "rsi", "rdx", "memory"
+		 : "rax", "rdi"
 		 );
 	return res;
 }
@@ -67,7 +67,7 @@ static __inline int64_t syscall_2(uint64_t n, uint64_t a1, uint64_t a2) {
 		"movq %%rax, %0\n\t"
 		 : "=r" (res)
 		 : "r" (n), "r" (a1), "r" (a2)
-		 : "rax", "rdi", "rsi", "rdx", "memory"
+		 : "rax", "rdi", "rsi"
 		 );
 	return res;
 }
@@ -84,7 +84,7 @@ static __inline int64_t syscall_3(uint64_t n, uint64_t a1, uint64_t a2, uint64_t
 		"movq %%rax, %0\n\t"
 		 : "=r" (res)
 		 : "r" (n), "r" (a1), "r" (a2), "r" (a3)
-		 : "rax", "rdi", "rsi", "rdx", "memory"
+		 : "rax", "rdi", "rsi", "rdx"
 		 );
 	return res;
 }
