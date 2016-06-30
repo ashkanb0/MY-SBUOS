8d8cb18cfa923cf30e41bc5ee3c69cc10d6f67df
ashkan <ashkan.dant3@gmail.com>
2016-06-30 02:19:03 -0400

    making kill binary

--- bin/kill/kill.c
+++ bin/kill/kill.c
@@ -1,9 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int atoi(const char* s){
+	int res = 0;
+	for (int i = 0; s[i] && i < 30; ++i)
+	{
+		res = (res*10)+(s[i] - '0');
+	}
+	return res;
+}
+
 
 int main(int argc, char const *argv[], char const *envp[])
 {
-	printf("KILL not implemented yet\n");
+	if(argc < 2){
+		printf("sleep: need an argumet\n");
+		return 1;
+	}
+	int pid = atoi(argv[1]);
+	if(kill(pid , 9))
+		printf("kill: no process found: %d\n", pid);
 	return 0;
 }
