d91fb7e6fb4038d78b21a020e7b5c12b4225ebf3
ashkan <ashkan.dant3@gmail.com>
2016-06-30 04:53:07 -0400

    completing read

--- sys/tarfs.c
+++ sys/tarfs.c
@@ -220,7 +220,7 @@ uint64_t read_to_buffer(fd_t* file_d, char* buffer ,uint64_t size){
 		buffer[size_read] = file[file_d->file_offset];
 		file_d -> file_offset ++;
 		if (buffer[size_read]=='\n'){
-			buffer[size_read] = '\0';
+			buffer[size_read+1] = '\0';
 			return size_read +1;
 		}
 	}
